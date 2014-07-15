c5be150b18962ad01fb4b1a3075290c1e2a0b894
BikTop <mantorovv@gmail.com>
2014-07-16 01:33:38 +0300

    http://demidovrealty.ru/

--- resume/vendor/jq.js
+++ resume/vendor/jq.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
-/*  $('.gallery li').css({
-    display: 'none'
-  });*/
   $(function(){
-    $('#slider2') // Demo 2 code, using FX full control
-      .anythingSlider({
-        resizeContents      : false,
-        navigationFormatter : function(i, panel){
-          return ['1', '2', '3', '4', '5', '6', '7', '8', '9'][i - 1];
-        }
+    $('#slider2').anythingSlider({
+      theme               : "default",
+      mode                : "fade",
+      expand              : false,
+      resizeContents      : false,
+      autoPlay            : true,
+      navigationFormatter : null
       })
   });
 
