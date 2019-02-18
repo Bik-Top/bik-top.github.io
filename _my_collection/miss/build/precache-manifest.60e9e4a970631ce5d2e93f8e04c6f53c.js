028c89ddc97de37cff0977e672dafb030c536a7d
BikTop <mantorovv@gmail.com>
2019-02-18 06:21:23 +0200

    miss

---
+++ _my_collection/miss/build/precache-manifest.60e9e4a970631ce5d2e93f8e04c6f53c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "27eb6ee4769b113e49ecc916c4cb24f2",
+    "url": "/static/media/item1.27eb6ee4.jpg"
+  },
+  {
+    "revision": "1f5ecadbf1804d9ed4f1a9f80202ea02",
+    "url": "/static/media/element.1f5ecadb.jpg"
+  },
+  {
+    "revision": "8eab9c72a09257b7f113861466c5841f",
+    "url": "/static/media/bg.8eab9c72.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "14f5a1f9d5918de29e35",
+    "url": "/static/js/main.14f5a1f9.chunk.js"
+  },
+  {
+    "revision": "adaa39e1f4f997c98c72",
+    "url": "/static/js/2.adaa39e1.chunk.js"
+  },
+  {
+    "revision": "14f5a1f9d5918de29e35",
+    "url": "/static/css/main.65fd965d.chunk.css"
+  },
+  {
+    "revision": "fc5434e3d3ff11f308d83459b7234a8b",
+    "url": "/index.html"
+  }
+];
